03ed587795bf9ba8eb906e93ffdcca1b464cec9e
Chayanin <Chayanin>
2017-09-11 00:22:47 +0700

    apply css on homepage

--- src/tryTen/settings.py
+++ src/tryTen/settings.py
@@ -124,6 +124,6 @@ if DEBUG:
     MEDIA_URL='/media/'
     STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","static-only")
     MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","media")
-    STATICFILE_DIRS=(
+    STATICFILES_DIRS=(
         os.path.join(os.path.dirname(BASE_DIR),"static","static"),
     )
