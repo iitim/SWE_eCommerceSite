8b426512572db8998136503787870b07d1af8a5c
Chayanin <Chayanin>
2017-09-11 04:59:03 +0700

    add profile page

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,4 +13,11 @@ def home(request):
 def about(request):
     context = locals()
     template = 'about.html'
+    return render(request, template, context)
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
     return render(request, template, context)
