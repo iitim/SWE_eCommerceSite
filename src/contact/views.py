3b5175f7e9c527b94e4e8c9fd464ac3eb57dfc3a
Chayanin <Chayanin>
2017-09-11 02:16:05 +0700

    add comfirm_message after filling the contact form

--- src/contact/views.py
+++ src/contact/views.py
@@ -9,7 +9,9 @@ from .forms import contactForm
 
 # Create your views here.
 def contact(request):
+    title = 'Contact'
     form = contactForm(request.POST or None)
+    context = {'title': title, 'form': form, }
 
     if form.is_valid():
         name = form.cleaned_data['name']
@@ -19,6 +21,8 @@ def contact(request):
         emailFrom = form.cleaned_data['email']
         emailTo = [settings.EMAIL_HOST_USER]
         send_mail( subject, massage, emailFrom, emailTo, fail_silently=True)
-    context = locals()
+        title = "Thanks!"
+        confirm_message = "Thanks for the message. We will get right back to you."
+        context = {'title': title, 'confirm_message': confirm_message, }
     template = 'contact.html'
     return render(request, template, context)
