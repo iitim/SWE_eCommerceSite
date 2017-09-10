78d940606df97631553e4ce0b32191537eeeaa0f
Chayanin <Chayanin>
2017-09-10 23:56:25 +0700

    custom homwpage

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
