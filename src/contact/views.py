3bb8bedb9b3d2b433598770c7926d65d0aea4a8b
Chayanin <Chayanin>
2017-09-11 02:01:05 +0700

    enable to contact with email

--- src/contact/views.py
+++ src/contact/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .forms import contactForm
 
@@ -10,7 +12,19 @@ def contact(request):
     form = contactForm(request.POST or None)
 
     if form.is_valid():
-        print request.POST
+        name = form.cleaned_data['name']
+        comment = form.cleaned_data['comment']
+        subject = 'Message from MYSITE.com'
+        massage = '%s %s' %(comment, name)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(
+    'Subject',
+    'massage',
+    'from@exxample.com',
+    ['to@exmple.com'],
+    fail_silently=False,
+)
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
